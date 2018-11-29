286a596b9395d36a4d9f260bc1c82995e9a02c10
maipham <maiphamt97@gmail.com>
2018-11-29 16:50:53 -0500

    'dfgd'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    SignUpComponent,
+    SignInComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
